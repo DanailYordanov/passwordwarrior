aacda4592aa76f7651cb1f525a87695efdea4b43
Danny Yordanov <danail.yordanov1@gmail.com>
2019-06-20 12:58:58 +0300

    Some frontend

--- passwordwarrior/users/forms.py
+++ passwordwarrior/users/forms.py
@@ -5,13 +5,20 @@ from django import forms
 class CustomLoginForm(LoginForm):
     def __init__(self, *args, **kwargs):
         super(CustomLoginForm, self).__init__(*args, **kwargs)
-        self.fields['login'].widget = forms.EmailInput(attrs={'class':'form-control','placeholder': 'Enter email or username'})
-        self.fields['password'].widget = forms.PasswordInput(attrs={'class':'form-control','placeholder': 'Enter password'})
+        self.fields['login'].widget = forms.EmailInput(
+            attrs={'class': 'form-control', 'placeholder': 'Enter email or username'})
+        self.fields['password'].widget = forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Enter password'})
+
 
 class CustomSignUpForm(SignupForm):
     def __init__(self, *args, **kwargs):
         super(CustomSignUpForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget = forms.TextInput(attrs={'class':'form-control','placeholder': 'Enter username'})
-        self.fields['email'].widget = forms.EmailInput(attrs={'class':'form-control','placeholder': 'Enter email'})
-        self.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control','placeholder': 'Enter password'})
-        self.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control','placeholder': 'Enter password again'})
+        self.fields['username'].widget = forms.TextInput(
+            attrs={'class': 'form-control', 'placeholder': 'Enter username'})
+        self.fields['email'].widget = forms.EmailInput(
+            attrs={'class': 'form-control', 'placeholder': 'Enter email'})
+        self.fields['password1'].widget = forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Enter password'})
+        self.fields['password2'].widget = forms.PasswordInput(
+            attrs={'class': 'form-control', 'placeholder': 'Enter password again'})
